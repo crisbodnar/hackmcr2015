f9bec397761eb0eb20f3016533f863ed0ead2a47
KasparMatas <kasparmat@hotmail.com>
2015-10-24 14:53:33 +0100

    Working function to get the middle of given coordinates in an array of coordinates.

--- Kaspars stuff/FunctionOfMidpoints.js
+++ Kaspars stuff/FunctionOfMidpoints.js
@@ -1,11 +1,11 @@
 var functionToGetMidpoints = function (arrayOfCoordinates) 
 {
 	var arrayOfMidpoints = [];
-	for (var selectedCoordinate = 0; selectedCoordinate < arrayOfCoordinates.length; selectedCoordinate ++)
+	for (var selectedCoordinate = 0; selectedCoordinate < arrayOfCoordinates.length-1; selectedCoordinate ++)
 	{
 		var coordinatesOfAMidpoint = [];
-		return1=(arrayOfCoordinates[selectedCoordinate[0]]+arrayOfCoordinates[selectedCoordinate+1[0]])/2;
-		return2=(arrayOfCoordinates[selectedCoordinate[1]]+arrayOfCoordinates[selectedCoordinate+1[1]])/2;
+		return1=((arrayOfCoordinates[selectedCoordinate][0])+(arrayOfCoordinates[selectedCoordinate+1][0]))/2;
+		return2=((arrayOfCoordinates[selectedCoordinate][1])+(arrayOfCoordinates[selectedCoordinate+1][1]))/2;
 		coordinatesOfAMidpoint.push(return1,return2);
 		arrayOfMidpoints.push(coordinatesOfAMidpoint);
 	}
