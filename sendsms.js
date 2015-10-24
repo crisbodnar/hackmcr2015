ec1d24d2485e5e3c198c53a8d4d04a75ba73c0a2
Pascal Jean-Francois <jeffpascal96@gmail.com>
2015-10-24 18:51:28 +0100

    no message

--- sendsms.js
+++ sendsms.js
@@ -1,14 +1,17 @@
-function sendsms(number) {
-
+function sendsms(number, message) {
+message = "hell";
+key = '6575b7a0c97c835c2125b5a59d4ad50da6f665d5';
 $.ajax({
-		key = '441234567890'
-        url= 'https://api.clockworksms.com/http/send.aspx';
-        data = 'key='+ key + '&to=' + number + '&content='+ message;
-
+		url:  'https://api.clockworksms.com/http/send.aspx',
+        data  : 'key='+ key + "&to=" + number + "&content="+ message,
+        success: function(data) {
+        	console.log(number);
+          console.log(data);
+        }
         //https://api.clockworksms.com/http/send.aspx?key=KEY&to=441234567890&content=Hello+World
       });
 }
 
 $( "#sms-button" ).click(function() {
-    sendsms(document.getElementById("number-input"))
+    sendsms(document.getElementById("number-input").value)
 });
