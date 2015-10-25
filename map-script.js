47e4530bbe7b9065d828f3e12d4ec21bf32f04f6
crisbodnar <crisbodnar96@gmail.com>
2015-10-25 01:35:23 +0100

    Pickup line works plus some bux fixes

--- map-script.js
+++ map-script.js
@@ -82,14 +82,14 @@
 		};
         // Create a map object and specify the DOM element for display.
 		var styles = [
-      {
+      /*{
         "featureType": "all",
 				"stylers": [
 					{
 						"color": "#18405b"
 					}
 				]
-      },
+      },*/
 			{
 				"featureType": "all",
 				"elementType": "labels.text.fill",
